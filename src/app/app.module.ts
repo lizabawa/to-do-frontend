4eb0998afa4504447f4388cf7b247740553cadc9
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-31 13:47:18 -0400

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { LandingComponent } from './landing/landing.component';
     LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: LandingComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
